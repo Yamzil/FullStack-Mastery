a0db71d1b33cff2f12b2fc5fc22f95328262120b
yamzil <yamzil@student.1337.ma>
2023-09-27 02:43:11 +0100

    solving getting appropriate ddata card

--- memory-card/src/cardgame.jsx
+++ memory-card/src/cardgame.jsx
@@ -1,18 +1,29 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import './index.css'
+import './index.css';
 
 const GameCard = () => {
-	const [pokemonArray, setpokemonArray] = useState([]);
-	let newArray = [...pokemonArray];
-	let currentIndex = newArray.length, temporaryValue;
-	let randomIndex = Math.floor(Math.random() * currentIndex);
+	const [pokemonArray, setPokemonArray] = useState([{
+		name: "",
+		id: 0,
+	}]);
+
+	const [score, setScore] = useState(0);
+
+	function handleScore() {
+		setScore(score + 1);
+	}
 
 	useEffect(() => {
-		axios.get('https://pokeapi.co/api/v2/pokemon?limit=100')
+		axios
+			.get('https://pokeapi.co/api/v2/pokemon?limit=5')
 			.then(response => {
 				const results = response.data.results;
-				setpokemonArray(results);
+				const pokemonData = results.map((pokemon, index) => ({
+					name: pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1),
+					id: index + 1,
+				}));
+				setPokemonArray(shuffleCards(pokemonData));
 			})
 			.catch(error => {
 				console.error('Error fetching Pokemon data:', error);
@@ -21,8 +32,10 @@ const GameCard = () => {
 
 	function shuffleCards(pokemonArray) {
 		let newArray = [...pokemonArray];
+		let currentIndex = newArray.length,
+			temporaryValue;
 		while (currentIndex !== 0) {
-			randomIndex = Math.floor(Math.random() * currentIndex);
+			let randomIndex = Math.floor(Math.random() * currentIndex);
 			currentIndex--;
 			temporaryValue = newArray[currentIndex];
 			newArray[currentIndex] = newArray[randomIndex];
@@ -31,21 +44,26 @@ const GameCard = () => {
 		return newArray;
 	}
 
+	function handleClickedCard() {
+		setPokemonArray(shuffleCards(pokemonArray))
+		handleScore();
+	}
 	return (
-		<div className='game'>
-			<div className='scoreboard'>
-				<span className='score'>Highest Score: </span>
+		<div className="game">
+			<div className="scoreboard">
+				<span className="score">Score: {score}</span>
+				<span className="score">Highest Score: </span>
 			</div>
-			<div className="game-card" onClick={() => setpokemonArray(shuffleCards(pokemonArray))}>
-				{pokemonArray.map((pokemon, index) => (
-					<div key={index} className="pokemon-card" >
+			<div className="game-card" onClick={handleClickedCard} >
+				{pokemonArray.map(pokemon => (
+					<div key={pokemon.id} className="pokemon-card">
 						<h2>{pokemon.name}</h2>
-						<img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${randomIndex}.png`}/>
+						<img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`} />
 					</div>
 				))}
 			</div>
 		</div>
 	);
-}
- 
+};
+
 export default GameCard;
